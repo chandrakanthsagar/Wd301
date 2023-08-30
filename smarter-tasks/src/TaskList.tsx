d97d1e2c9918167d1395051067056c63270da4d3
chandrakanthsagar <109515216+chandrakanthsagar@users.noreply.github.com>
2023-08-30 18:15:40 +0530

    level 3 first commit

---
+++ smarter-tasks/src/TaskList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Task from "./Task";
+import { TaskItem } from "./types";
+interface Props {
+    tasks: TaskItem[];
+}
+
+
+interface State {
+  tasks: TaskItem[];
+}
+
+
+class TaskList extends React.Component<Props,State> {
+    
+      
+    render() {
+        return this.props.tasks.map((task, idx) => (
+          <Task key={idx} title={task.title} />
+        ));
+      }
+    
+}
+
+export default TaskList;
